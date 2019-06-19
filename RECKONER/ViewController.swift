ace00d310243a588444e17be52c39fad2f3056f2
Siem Debesay <siemd2@learn.cssd.ab.ca>
2019-06-19 12:43:54 -0600

    this is my one push to GitHub once i gained access to the Mac lab and can create a new remote bc it wasn't letting me do that on my VM Mac OS
    most recent update:
    	-added readme file
    	-corrected a scope error
    	-tried to make code as streamlined as possible thus grouped all the UIbuttons to a single function and differentiate them via sender.tag as opposed to having so many different UIbuttons which may clutter code
    	-nothing special, just tried flushing out the absolute basics
    
    aside from that the app works as it should.

--- RECKONER/ViewController.swift
+++ RECKONER/ViewController.swift
@@ -13,34 +13,47 @@ class ViewController: UIViewController {
     var numberOnScreen:Double = 0
     var previousNumber:Double = 0
     var performingMath = false
+    var operation = 0
     
     @IBOutlet weak var label: UILabel!
     
-    @IBAction func numbers(_ sender: UIButton)
-    {
-        label.text = label.text! + String(sender.tag-1)
-        numberOnScreen = Double(label.text!)!
+    @IBAction func numbers(_ sender: UIButton) {
+        
+        if performingMath == true {
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
+        }
+        else {
+            
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+        }
     }
     
     
-    @IBAction func buttons(_ sender: UIButton)
-    {
+    @IBAction func buttons(_ sender: UIButton) {
+        
         if label.text != "" && sender.tag != 11 && sender.tag != 16 {
             
+            previousNumber = Double(label.text!)!
             
             // divide operation
-            if sender.tag == 12{
+            if sender.tag == 12 {
                 
+                label.text = "/"
             }
                 
             // multiplication operation
             else if sender.tag == 13 {
                 
+                label.text = "X"
             }
                 
             // subtraction operation
             else if sender.tag == 14 {
                 
+                label.text = "-"
             }
                 
             //addition operation
@@ -48,8 +61,32 @@ class ViewController: UIViewController {
                 
                 label.text = "+"
             }
-            
+            operation = sender.tag
             performingMath = true
+        }
+        // calculations done when sender.tag == 16 i.e = is pressed and user wants to calculate their results
+        else if sender.tag == 16 {
+            
+                // division operation once = is pressed
+            if operation == 12 {
+                label.text = String(previousNumber / numberOnScreen)            }
+                // multiplication operation once = is pressed
+            else if operation == 13 {
+                label.text = String(previousNumber * numberOnScreen)            }
+                // subtraction operation once = is pressed
+            else if operation == 14 {
+                label.text = String(previousNumber - numberOnScreen)            }
+                // addition operation once = is pressed
+            else if operation == 15 {
+                label.text = String(previousNumber + numberOnScreen)
+            }
+        }
+        else if sender.tag == 11 {
+            numberOnScreen = 0
+            previousNumber = 0
+            operation = 0
+            label.text = ""
+            
         }
     }
     
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Reckoner Final Project
+
+Name: Siem Debesay
+
+ID: siemd2
+
+Date Completed: June 19th, 2019
